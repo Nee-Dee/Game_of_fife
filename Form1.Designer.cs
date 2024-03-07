5d6555eb18972e99658ea4be6909fe9cb0f409ae
Nee-Dee <smikov45@gmail.com>
2024-03-07 19:25:08 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace Game_of_fife
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.numericUpDown2);
+            this.splitContainer1.Panel1.Controls.Add(this.checkBox3);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.button4);
+            this.splitContainer1.Panel1.Controls.Add(this.button3);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.checkBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.checkBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.numericUpDown1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(941, 542);
+            this.splitContainer1.SplitterDistance = 115;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(11, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 39);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Плотность населения";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(21, 124);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(71, 23);
+            this.numericUpDown2.TabIndex = 10;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            27,
+            0,
+            0,
+            0});
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(11, 257);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(57, 19);
+            this.checkBox3.TabIndex = 9;
+            this.checkBox3.Text = "Сетка";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(11, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 31);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Режим размещения";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Размер клетки";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(9, 486);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 35);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Правила";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(9, 445);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "О программе";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(9, 333);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 35);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Стоп";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(11, 221);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(71, 19);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Самому";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(11, 196);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(81, 19);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Случайно";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(21, 48);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(71, 23);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(820, 540);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 542);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game of Life";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_of_fife
+{
+	public partial class Form1 : Form
+	{
+		private Graphics gr;
+		private int cell;
+		private bool[,] field;
+		private int row;
+		private int col;
+		private int q = 0;
+		private int started = 0;
+		private int gen = 0;
+		private int grid = 0;
+		private int next_gen_cells_count = 0;
+		private int cells_count = 0;
+		private bool mode = true;
+
+		public Form1()
+		{
+			InitializeComponent();
+			pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+			gr = Graphics.FromImage(pictureBox1.Image);
+		}
+
+		private void newGame(int cell, int col, int row)
+		{
+			field = new bool[col, row];
+			Random rand = new Random();
+			for (int i = 0; i < col; i++)
+			{
+				for (int j = 0; j < row; j++)
+				{
+					field[i, j] = rand.Next(30 - (int)numericUpDown2.Value) == 0;
+					if (field[i, j] == true)
+						cells_count++;
+				}
+			}
+			timer1.Start();
+		}
+
+		private void myself(int cell, int col, int row)
+		{
+			if (q == 0)
+				field = new bool[col, row];
+			timer1.Start();
+		}
+
+		private void nextGen()
+		{
+			gr.Clear(Color.White);
+			var nextfield = new bool[col, row];
+			for (int i = 0; i < col; i++)
+			{
+				for (int j = 0; j < row; j++)
+				{
+					var c = count(i, j);
+					var alive = field[i, j];
+					if (!alive && c == 3)
+					{
+						nextfield[i, j] = true;
+						next_gen_cells_count++;
+					}
+					else if (alive && (c < 2 || c > 3))
+					{
+						nextfield[i, j] = false;
+						next_gen_cells_count--;
+					}
+					else nextfield[i, j] = field[i, j];
+					if (alive)
+					{
+						if (checkBox3.Checked == true)
+							grid = 1;
+						else grid = 0;
+						gr.FillRectangle(Brushes.Black, i * cell, j * cell, cell - grid, cell - grid);
+					}
+				}
+			}
+			field = nextfield;
+			pictureBox1.Refresh();
+			Text = $"Game of Life: Поколение {++gen}  {cells_count}";
+		}
+
+		private int count(int x, int y)
+		{
+			int k = 0;
+			for (int ki = -1; ki < 2; ki++)
+				for (int kj = -1; kj < 2; kj++)
+				{
+					var kx = (x + ki + col) % col;
+					var ky = (y + kj + row) % row;
+					var check = kx == x && ky == y;
+					var alive = field[kx, ky];
+					if (alive && !check)
+						k++;
+				}
+			return k;
+		}
+
+		private void checkBox1_CheckedChanged(object sender, EventArgs e)
+		{
+			if (checkBox1.Checked == true)
+			{
+				checkBox2.Checked = false;
+				mode = true;
+				gr.Clear(Color.White);
+				pictureBox1.Refresh();
+			}
+			if (checkBox1.Checked == false)
+			{
+				gr.Clear(Color.White);
+				checkBox2.Checked = true;
+				mode = false;
+				pictureBox1.Refresh();
+			}
+		}
+
+		private void checkBox2_CheckedChanged(object sender, EventArgs e)
+		{
+			if (checkBox2.Checked == true)
+			{
+				checkBox1.Checked = false;
+				mode = false;
+			}
+			if (checkBox2.Checked == false)
+			{
+				checkBox1.Checked = true;
+				mode = true;
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (button1.Text == "Старт")
+			{
+				if (mode)
+					field = new bool[col, row];
+				gen = 0;
+				Text = $"Game of Life: Поколение {gen}";
+				numericUpDown1.Enabled = false;
+				numericUpDown2.Enabled = false;
+				button1.Text = "Пауза";
+				checkBox1.Enabled = false;
+				checkBox2.Enabled = false;
+				button2.Enabled = true;
+				if (mode)
+					newGame(cell, col, row);
+				else
+					myself(cell, col, row);
+			}
+			else if (button1.Text == "Пауза")
+			{
+				timer1.Stop();
+				button1.Text = "Продолжить";
+				checkBox3.Enabled = false;
+			}
+			else if (button1.Text == "Продолжить")
+			{
+				timer1.Start();
+				button1.Text = "Пауза";
+				checkBox3.Enabled = true;
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			q = 1;
+			cells_count = 0;
+			next_gen_cells_count = 0;
+			gr.Clear(Color.White);
+			button2.Enabled = false;
+			numericUpDown1.Enabled = true;
+			numericUpDown2.Enabled = true;
+			checkBox1.Enabled = true;
+			checkBox2.Enabled = true;
+			checkBox3.Enabled = true;
+			button1.Text = "Старт";
+			timer1.Stop();
+			pictureBox1.Refresh();
+			field = new bool[col, row];
+			Text = $"Game of Life";
+		}
+
+		private bool mouse(int x, int y)
+		{
+			return x >= 0 && y >= 0 && x < col && y < row;
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			nextGen();
+		}
+
+		private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (mode)
+				return;
+			if (q==0)
+			{
+				field = new bool[col, row];
+				q++;
+			}
+			if (e.Button == MouseButtons.Left)
+			{
+				numericUpDown1.Enabled = false;
+				var x = e.Location.X / cell;
+				var y = e.Location.Y / cell;
+				var mouse_pos = mouse(x, y);
+				if (mouse_pos)
+				{
+					field[x, y] = true;
+					if (checkBox3.Checked == true)
+						grid = 1;
+					else grid = 0;
+					gr.FillRectangle(Brushes.Black, x * cell, y * cell, cell - grid, cell - grid);
+				}
+			}
+			if (e.Button == MouseButtons.Right)
+			{
+				numericUpDown1.Enabled = false;
+				var x = e.Location.X / cell;
+				var y = e.Location.Y / cell;
+				var mouse_pos = mouse(x, y);
+				if (mouse_pos)
+				{
+					field[x, y] = false;
+					gr.FillRectangle(Brushes.White, x * cell, y * cell, cell, cell);
+				}
+			}
+			if (!timer1.Enabled)
+			{
+				pictureBox1.Refresh();
+			}
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Игра «Жизнь» создана в Microsoft Visual C#.\nСмыков Андрей Алексеевич, " +
+				"Курганский Государственный Университет, «Программная инженерия», преподаватель - Медведев Аркадий Андреевич.\nКурган, 2021 год.", "О программе", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Игра «Жизнь»\n\nВообразите бесконечное поле, разделенное на клетки. " +
+				"На каждой клетке поля живет, рождается или погибает животное. Это зависит от условий Среды, т.е.от того, сколько соседей " +
+				"у него на ближайших восьми клетках(четырех по сторонам и четырех по углам).\n Действуют три правила существования животных:\n  " +
+				"1.Каждое животное, у которого два или три соседа, живет и сохраняется до следующего поколения.\n  2.Животное погибает, если " +
+				"у него больше трех соседей (от недостатка места), совсем их нет или только один сосед (от одиночества).\n  3.Когда рядом с " +
+				"какой - нибудь клеткой есть три животных(соседа), то на этой клетке рождается новое животное.\n  4.Важно понять, что животные " +
+				"погибают и рождаются одновременно.   Они образуют одно поколение.\n  За один ход в игре в соответствии с упомянутыми правилами " +
+				"осуществляется переход от одного поколения к другому.  По умолчанию стоит режим со случайным количеством животных на случайных " +
+				"клетках поля. При желании можно поменять режим распределения животных и расставить их самому. Нажатие левой кнопкой мыши на пустую " +
+				"клетку сделает её закрашенной, а нажатие правой кнопкой на закрашенную сделает её пустой. Также можно поменять плотность распределения " +
+				"животных при помощи соответствующего пункта настроек. Он может принимать значения от 30 до 1. Чем больше значение плотности населения," +
+				" тем меньше будет свободных клеток и наоборот.", "Правила", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			if (started == 0)
+			{
+				started++;
+				field = new bool[col, row];
+				cell = (int)numericUpDown1.Value;
+				col = pictureBox1.Width / cell;
+				row = pictureBox1.Height / cell;
+			}
+		}
+
+		private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+		{
+			cell = (int)numericUpDown1.Value;
+			col = pictureBox1.Width / cell;
+			row = pictureBox1.Height / cell;
+			field = new bool[col, row];
+		}
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+			if (mode)
+				return;
+			if (q == 0)
+			{
+				field = new bool[col, row];
+				q++;
+			}
+			if (e.Button == MouseButtons.Left)
+			{
+				numericUpDown1.Enabled = false;
+				var x = e.Location.X / cell;
+				var y = e.Location.Y / cell;
+				var mouse_pos = mouse(x, y);
+				if (mouse_pos)
+				{
+					field[x, y] = true;
+					if (checkBox3.Checked == true)
+						grid = 1;
+					else grid = 0;
+					gr.FillRectangle(Brushes.Black, x * cell, y * cell, cell - grid, cell - grid);
+				}
+			}
+			if (e.Button == MouseButtons.Right)
+			{
+				numericUpDown1.Enabled = false;
+				var x = e.Location.X / cell;
+				var y = e.Location.Y / cell;
+				var mouse_pos = mouse(x, y);
+				if (mouse_pos)
+				{
+					field[x, y] = false;
+					gr.FillRectangle(Brushes.White, x * cell, y * cell, cell, cell);
+				}
+			}
+			if (!timer1.Enabled)
+			{
+				pictureBox1.Refresh();
+			}
+		}
+    }
+}
